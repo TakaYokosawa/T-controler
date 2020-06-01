f104c277939b0e9bb7c7aa9314cf42284e68ce8e
TakaYokosawa <tyokosawa@g.ecc.u-tokyo.ac.jp>
2020-06-01 15:02:57 +0900

    add timer

--- Omron_Temperature_Controller.py
+++ Omron_Temperature_Controller.py
@@ -25,11 +25,16 @@ class main_window(QtWidgets.QMainWindow, Ui_OmronMainWindow):
         self.actionAbout_Hardware.triggered.connect(self.show_hardware_info)
         
         self.Power_button.toggled.connect(self.power_toggle)
+        self.Timer_button.toggled.connect(self.timer_toggle)
         self.SetPoint_control.valueChanged.connect(self.set_setpoint)
+        self.Timer_control.timeChanged.connect(self.set_timer)
+        self.set_timer()
         self.get_setpoint()
         self.run_pv_indicator()
         self._is_power_on = True
+        self._is_timer_on = False        
         self.power_on()
+        self.timer_off()
         
     def close(self):
         self._is_subprocess_running = False
@@ -43,6 +48,15 @@ class main_window(QtWidgets.QMainWindow, Ui_OmronMainWindow):
             pv_str = "{0:.1f}".format(pv)
             self.PV_indicator.display(pv_str)
             time.sleep(0.5)
+
+    def proceed_timer(self):
+        while self._is_timer_on:
+            time.sleep(1)
+            self._remaining_time = self._remaining_time.addSecs(-1)
+            self.Timer_indicator.setText(self._remaining_time.toString())
+            if self._remaining_time == QtCore.QTime(0, 0, 0):
+                self.power_toggle()
+                self.timer_toggle()
             
     def get_setpoint(self):
         self._rlock.acquire()
@@ -55,6 +69,10 @@ class main_window(QtWidgets.QMainWindow, Ui_OmronMainWindow):
     def run_pv_indicator(self):
         self.pv_thread = threading.Thread(target=self.get_pv)
         self.pv_thread.start()
+
+    def run_timer(self):
+        self.timer_thread = threading.Thread(target=self.proceed_timer)
+        self.timer_thread.start()
         
     def set_setpoint(self):
         setpoint = self.SetPoint_control.value()
@@ -62,6 +80,10 @@ class main_window(QtWidgets.QMainWindow, Ui_OmronMainWindow):
         self.omron.set_setpoint(setpoint)
         self._rlock.release()
         self.get_setpoint()
+
+    def set_timer(self):
+        self._remaining_time = self.Timer_control.time()
+        self.Timer_indicator.setText(self._remaining_time.toString())
         
     def power_on(self):
         self._rlock.acquire()
@@ -69,18 +91,32 @@ class main_window(QtWidgets.QMainWindow, Ui_OmronMainWindow):
         self._rlock.release()
         self.Power_button.setStyleSheet("background-color:rgb(255,0,0)")
 
+    def timer_on(self):
+        self.Timer_button.setStyleSheet("background-color:rgb(255,0,0)")
+        self.run_timer()
+
     def power_off(self):
         self._rlock.acquire()
         self.omron.stop()
         self._rlock.release()
         self.Power_button.setStyleSheet("background-color:rgb(0,255,0)")
-    
+
+    def timer_off(self):
+        self.Timer_button.setStyleSheet("background-color:rgb(0,255,0)")
+
     def power_toggle(self):
         self._is_power_on = not self._is_power_on
         if self._is_power_on:
             self.power_on()
         else:
             self.power_off()
+
+    def timer_toggle(self):
+        self._is_timer_on = not self._is_timer_on
+        if self._is_timer_on:
+            self.timer_on()
+        else:
+            self.timer_off()
         
     def show_hardware_info(self):
         message = "{hardware: <17} : {product: <40}\n".format(hardware = "Controller", product = "Omron E5CC-QX3A5M-003") + \
